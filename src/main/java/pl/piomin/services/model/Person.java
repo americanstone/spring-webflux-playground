1786917b5c84674851665a50d401ff4cc05b1f01
Gordon zhang <gordon.zhang@coxautoinc.com>
2021-12-21 10:05:04 -0500

    add gradle

--- src/main/java/pl/piomin/services/model/Person.java
+++ src/main/java/pl/piomin/services/model/Person.java
@@ -5,7 +5,6 @@ import lombok.*;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 @ToString
 public class Person {
 
@@ -14,4 +13,42 @@ public class Person {
     private String lastName;
     private int age;
 
+    public Person(Integer id, String firstName, String lastName, int age) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
